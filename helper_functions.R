88c2d33548c4ad35c2f32f1b3f1e65cf31a89655
Peter Stewart <44345293+Peter-Stewart@users.noreply.github.com>
2022-07-05 13:42:00 +0100

    add sites-as-days support to generate_distance_matrix

--- helper_functions.R
+++ helper_functions.R
@@ -70,10 +70,42 @@ generate_detection_matrix_hours <- function(sp, binary = FALSE){
 
 
 # Generates distance matrix from columns that contain "lat" or "long"
-generate_distance_matrix <- function(df, center = FALSE, rescale = FALSE){
+generate_distance_matrix <- function(df, center = FALSE, rescale = FALSE, sites_as_days = FALSE){
   
   # Select columns which contain "long" or "lat" in their name
   coords <- df %>% select(contains("long") | contains("lat"))
+  
+  # Return error if more than two columns
+  if(length(coords) > 2){
+    stop("More than two coordinate columns - make sure dataframe contains lat and long coordinates only")
+  }
+  
+  if(sites_as_days == TRUE){
+    if(!exists("sitedays")){
+      stop("Please load the sitedays dataframe")
+    }
+    
+    sites <- df %>% select(contains("site") & !contains("description"))
+    colnames(sites) <- c("Site")
+    coords <- cbind(sites, coords)
+    coords <- merge(coords, sitedays, by = "Site")
+    ff <- coords %>% uncount(Days)
+    ff$Site <- ifelse(ff$Site < 10, paste0("0", ff$Site), ff$Site)
+    ff$Site <- paste0("Site_",ff$Site)
+    gg <- rep(NA, nrow(ff))
+    gg[1] <- 1
+    ff <- cbind(ff,gg)
+    for(i in 2:nrow(ff)){
+      if(ff$Site[i]==ff$Site[i-1])
+        ff$gg[i] <-  ff$gg[i-1] + 1L 
+      else
+        ff$gg[i] <- 1L
+    }
+    ff$site_day <- paste0(ff$Site,"_",ff$gg)
+    coords <- ff %>% select(-Site, -gg)
+    rownames(coords) <- coords$site_day
+    coords <- coords %>% select(-site_day)
+  }
   
   # If center is true, center around zero
   if(center == TRUE & rescale == FALSE){
