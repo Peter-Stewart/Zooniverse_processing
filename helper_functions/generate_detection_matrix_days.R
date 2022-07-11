28fc0cb8ee373b85e5aebe754967d1893cfb4200
Peter Stewart <44345293+Peter-Stewart@users.noreply.github.com>
2022-07-11 15:00:22 +0100

    Create generate_detection_matrix_days.R
    partially complete

---
+++ helper_functions/generate_detection_matrix_days.R
@@ -0,0 +1,46 @@
+# Generates detection matrix where the rows are sites and the columns are days
+
+generate_detection_matrix_hours <- function(sp, binary = FALSE){
+  
+  # Check if consensus classifications dataframe exists, stop and warn if not
+  if(!exists("consensus_classifications")){
+    stop("Please load the consensus_classifications dataframe")
+  }
+  
+  # Check if sitedays dataframe exists, stop and warn if not
+  if(!exists("startends")){
+    stop("Please load the startends dataframe")
+  }
+  
+  # Subset to the correct species
+  df <- consensus_classifications %>% filter(species==sp)
+  
+  # Warn and return NA if species was never observed 
+  if(nrow(df)==0){
+    warning(paste(sp, "was never observed"))
+    return(NA)
+  }
+  
+  # Add indicator for use later
+  df$indicator <- 1L
+  
+  # Prepare startends dataframe into sites_k
+  sites_k <- startends %>% select(Site, Deploy_date_lub, Days) %>% 
+    filter(!is.na(Days)) %>%
+    uncount(Days)
+  sites_k$Site <- ifelse(sites_k$Site < 10, paste0("0", sites_k$Site), sites_k$Site)
+  sites_k$Site <- paste0("Site_",sites_k$Site)
+  k <- rep(NA, nrow(sites_k))
+  k[1] <- 1
+  sites_k <- cbind(sites_k,k)
+  for(i in 2:nrow(sites_k)){
+    if(sites_k$Site[i]==sites_k$Site[i-1])
+      sites_k$k[i] <-  sites_k$k[i-1] + 1L 
+    else
+      sites_k$k[i] <- 1L
+  }
+  sites_k$k_date <- sites_k$Deploy_date_lub + (sites_k$k - 1)
+
+  # Merge sites_k with consensus classifications so we know which k day the detection was on
+  
+}
