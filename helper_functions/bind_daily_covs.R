a21d7d2f2ba4293320bba30b6c3f2ddc0525d7a4
Peter Stewart <44345293+Peter-Stewart@users.noreply.github.com>
2022-11-07 12:59:07 +0000

    Update bind_daily_covs.R
    now works!

--- helper_functions/bind_daily_covs.R
+++ helper_functions/bind_daily_covs.R
@@ -1,24 +1,14 @@
 bind_daily_covs <- function(startends, day_data, day_cov, summary_type = "mean", date_format = "ymd"){
   
-  # Stop and warn if date column not found
-  if("Date" %notin% colnames(day_data)){
-    stop("Error: date column not found. Please make sure column is named 'Date'.")
-  }
-  
-  # Stop and warn if date column is in wrong format
-  if(!is.Date(day_data$Date){
-    stop("Error: wrong date format. Please make sure column is in lubridate's Date format.")
-  }
-  
   # Select date format (default is year-month-day)
   if(date_format == "dmy"){
-    warn("Using day-month-year date format. Set date_format argument to change.")
+    warning("Using day-month-year date format. Set date_format argument to change.")
     day_data$DateLub <- dmy(weather$Date)
   }else if(date_format == "mdy"){
-    warn("Using month-day-year date format. Set date_format argument to change.")
+    warning("Using month-day-year date format. Set date_format argument to change.")
     day_data$DateLub <- mdy(weather$Date)
   }else if(date_format == "ymd"){
-    warn("Using year-month-day date format. Set date_format argument to change.")
+    warning("Using year-month-day date format. Set date_format argument to change.")
     day_data$DateLub <- ymd(weather$Date)
   }
   
@@ -27,19 +17,19 @@ bind_daily_covs <- function(startends, day_data, day_cov, summary_type = "mean",
     
   # Group and summarise based on the selected option  
   if(summary_type == "mean"){
-    cov_daily <- cov_daily %>% group_by(DateLub) %>% summarise(var = mean(day_cov))
+    cov_daily <- cov_daily %>% group_by(DateLub) %>% summarise(var = mean(!!sym(day_cov)))
   }else if(summary_type == "median"){
-    cov_daily <- cov_daily %>% group_by(DateLub) %>% summarise(var = median(day_cov))
+    cov_daily <- cov_daily %>% group_by(DateLub) %>% summarise(var = median(!!sym(day_cov)))
   }else if(summary_type == "total"){
-    cov_daily <- cov_daily %>% group_by(DateLub) %>% summarise(var = sum(day_cov))
+    cov_daily <- cov_daily %>% group_by(DateLub) %>% summarise(var = sum(!!sym(day_cov)))
   }else if(summary_type == "min"){
-    cov_daily <- cov_daily %>% group_by(DateLub) %>% summarise(var = min(day_cov))
+    cov_daily <- cov_daily %>% group_by(DateLub) %>% summarise(var = min(!!sym(day_cov)))
   }else if(summary_type == "max"){
-    cov_daily <- cov_daily %>% group_by(DateLub) %>% summarise(var = max(day_cov))
+    cov_daily <- cov_daily %>% group_by(DateLub) %>% summarise(var = max(!!sym(day_cov)))
   }
   
-  cov_daily <- cov_daily %>% rename(paste0(day_cov,"_",summary_type) = var)
-  
+  cov_daily <- cov_daily %>% rename_with(.fn = ~paste0(day_cov,"_",summary_type), .cols = "var")
+
   # Create dataframe with date for each visit (k)
   sites_k <- startends %>% select(Site, Deploy_date_lub, Days) %>% 
     filter(!is.na(Days)) %>%
@@ -60,6 +50,8 @@ bind_daily_covs <- function(startends, day_data, day_cov, summary_type = "mean",
   
   # Merge by date
   k_covs <- merge(sites_k, cov_daily, by.x = "k_date", by.y = "DateLub", all.x = TRUE)
+  
+  k_covs <- k_covs %>% select(-Deploy_date_lub)
   
   # Return k_covs
   return(k_covs)
